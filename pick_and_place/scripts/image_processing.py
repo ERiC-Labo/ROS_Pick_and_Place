1465db3cc876d117fe9b711635c30e46b2902eaf
KeisukeHamamoto <keihama@gmail.com>
2022-06-18 23:54:09 +0900

    2022/06/28

--- pick_and_place/scripts/image_processing.py
+++ pick_and_place/scripts/image_processing.py
@@ -6,9 +6,6 @@ from cv_bridge import CvBridge
 import cv2
 import numpy as np
 
-
-
-
 # コールバック関数
 def process_image(msg):
     try:
@@ -19,22 +16,24 @@ def process_image(msg):
         img = cv2.cvtColor(original, cv2.COLOR_BGR2HSV)
         # マスク画像作成
         mask_img = cv2.inRange(img, (0, 127, 0), (10, 255, 255))
+        # 重心座標計算
         mu = cv2.moments(mask_img, False)
         x, y = int(mu["m10"]/mu["m00"]), int(mu["m01"]/mu["m00"])
+        # ピクセル座標系からワールド座標系に変換
         X = (x-640/2)*(1.24)/381.362467 + 0.3
-        Y = (y-360/2)*(1.24)/381.362467
+        Y = -(y-360/2)*(1.24)/381.362467
         Z = 1.0
-        
+        # 把持位置を格納
         picking_point = Point()
         picking_point.x = X
-        picking_point.y = -Y
+        picking_point.y = Y
         picking_point.z = Z
         # opencv型からImage型のmsgに変換
         imgMsg = bridge.cv2_to_imgmsg(mask_img, "mono8")
-        
-        # publish
-        pub = rospy.Publisher('mask_img', Image, queue_size=10)
+        # マスク画像をpublish
+        pub = rospy.Publisher('/mask_img', Image, queue_size=10)
         pub.publish(imgMsg)
+        # 把持位置をpublish
         position_pub = rospy.Publisher('/picking_position', Point, queue_size = 10)
         position_pub.publish(picking_point)
     except Exception as err:
@@ -44,7 +43,7 @@ def start_node():
     # nodeの初期化
     rospy.init_node('img_proc')
     rospy.loginfo('img_proc node started')
-    # subscribeする
+    # 画像をシミュレーションからsubscribeする
     rospy.Subscriber("/camera/image_raw", Image, process_image)
     rospy.spin()
 
--- pick_and_place/src/pick_and_place_class.cpp
+++ pick_and_place/src/pick_and_place_class.cpp
@@ -3,33 +3,47 @@
 Pick_and_Place::Pick_and_Place(ros::NodeHandle& node_handle)
     :   manipulator("manipulator"),
         gripper("gripper"){
+            // 把持位置をsubscribeし、把持を実行
             sub = node_handle.subscribe("/picking_position", 1000, &Pick_and_Place::DoPickAndPlace, this);
         }
 
 void Pick_and_Place::DoPickAndPlace(geometry_msgs::Point::ConstPtr const &msg)
 {
+    // 初期位置に移動
     manipulator.setJointValueTarget(manipulator.getNamedTargetValues("home"));
     manipulator.move();
+    // グリッパーを開く
     gripper.setJointValueTarget(gripper.getNamedTargetValues("open"));
     gripper.move();
+    // ロボットハンドの姿勢を保存
     geometry_msgs::PoseStamped current_pose;
     current_pose = manipulator.getCurrentPose("ee_link");
+    // subscribeした把持位置に移動
     MoveManipulator(msg->x, msg->y, msg->z, current_pose);
+    // 対象物体にアプローチ
     MoveDown(0.11);
+    // グリッパーを閉じて対象物体を把持
     gripper.setJointValueTarget(gripper.getNamedTargetValues("closed"));
     gripper.move();
+    // 一秒待機
     ros::Duration(1.0).sleep();
+    // 垂直方向に移動
     MoveUp(0.11);
+    // 箱の上まで移動
     MoveManipulator(0.0, 0.8, 0.8, current_pose);
+    // グリッパーを開いて対象物体を離す
     gripper.setJointValueTarget(gripper.getNamedTargetValues("open"));
     gripper.move();
+    // 一秒待機
     ros::Duration(1.0).sleep();
+    // 初期位置に移動
     manipulator.setJointValueTarget(manipulator.getNamedTargetValues("home"));
     manipulator.move();
     sub.shutdown();
     
 }
 
+// マニピュレータを動かす関数
 void Pick_and_Place::MoveManipulator(float x, float y, float z, geometry_msgs::PoseStamped pose)
 {
     geometry_msgs::Pose target_pose;
@@ -46,6 +60,7 @@ void Pick_and_Place::MoveManipulator(float x, float y, float z, geometry_msgs::P
     manipulator.execute(trajectory);
 }
 
+// マニピュレータを垂直に下げる関数
 void Pick_and_Place::MoveDown(float z)
 {
     std::vector<geometry_msgs::Pose> waypoints;
@@ -59,6 +74,7 @@ void Pick_and_Place::MoveDown(float z)
     manipulator.execute(trajectory);
 }
 
+// マニピュレータを垂直に上げる関数
 void Pick_and_Place::MoveUp(float z)
 {
     std::vector<geometry_msgs::Pose> waypoints;
